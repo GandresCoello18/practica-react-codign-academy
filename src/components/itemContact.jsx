3872d017d653199795a43c227a811a951f035ab3
GandresCoello18 <goyeselcoca@gmail.com>
2025-07-14 22:31:25 -0500

    crear ejercicio de lista de contactos

---
+++ src/components/itemContact.jsx
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @component
+ * @param {Object} props - Parámetros que recibe mi componente
+ * @param {number} props.indexContact - Indice del contacto
+ * @param {{  name: string, phone: string }} props.data - Datos del contacto
+ * @param {Function} props.onClickEdit - Función para editar un contacto
+ * @param {Function} props.onClickDelete - Función para eliminar un contacto
+ * @returns {JSX.Element} - El componente ItemContact
+ */
+export const ItemContact = ({ indexContact, data, onClickEdit, onClickDelete }) => {
+    return (
+        <li>{indexContact + 1} / {data.name} - {data.phone} <span className='edit' onClick={() => onClickEdit(indexContact)}>#</span> <span className='close' onClick={() => onClickDelete(indexContact)}>X</span></li>
+    )
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginView } from '../view/Login';
 import { HomeView } from '../view/Home';
 import { NotFoundView } from '../view/404';
+import { ContactView } from '../view/contacto';
 
 export const RouterApp = () => {
   return (
@@ -9,6 +10,7 @@ export const RouterApp = () => {
       <Routes>
         <Route path="/login" element={<LoginView />} />
         <Route path="/home" element={<HomeView />} />
+        <Route path="/contacts" element={<ContactView />} />
         <Route path="*" element={<NotFoundView />} />
       </Routes>
     </BrowserRouter>
