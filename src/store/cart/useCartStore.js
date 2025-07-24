c7f6b0d1e1a0512725fca07fa745ee6098cf5afa
GandresCoello18 <goyeselcoca@gmail.com>
2025-07-23 22:08:10 -0500

    add devtools in zustand

--- src/store/cart/useCartStore.js
+++ src/store/cart/useCartStore.js
@@ -1,14 +1,16 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, devtools } from 'zustand/middleware';
 
-export const useCartStore = create(persist(
-    set => ({
-        cart: [],
+export const useCartStore = create(devtools(
+    persist(
+        set => ({
+            cart: [],
 
-        addToCart: (product) => set(state => ({ cart: [...state.cart, product] })),
-        removeToCart: (productId) => set(state => ({ cart: state.cart.filter(product => product.id !== productId) })),
-        clearCart: () => set(() => ({ cart: [] }))
-    })
-), {
-    name: 'cart-products'
-});
+            addToCart: (product) => set(state => ({ cart: [...state.cart, product] })),
+            removeToCart: (productId) => set(state => ({ cart: state.cart.filter(product => product.id !== productId) })),
+            clearCart: () => set(() => ({ cart: [] }))
+        })
+    ), {
+        name: 'cart-products'
+    }
+));
